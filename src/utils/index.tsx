c7112dca52b5015ee74dbfe45f210a956f389ebb
Mkparu <mkparusomtochi26@gmail.com>
2023-07-28 22:32:10 +0100

    fix(utils): resolve course link issue.

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,21 +1,21 @@
 const formatLink = (text:string)=>
   {
-    const courseLink = 'courses';
+    const courseLink = '/courses';
     switch (text) {
         case 'About us':
             return 'about'
         case 'Contact':
             return 'contact'
         case 'Product Design':
-          return courseLink + "";
+          return courseLink + "/1";
         case 'Product Management':
-          return courseLink + "";
+          return courseLink + "/4";
         case '3D':
-          return courseLink + "";
+          return courseLink + "/5";
         case 'Full Stack Javascript':
-          return courseLink + "";
+          return courseLink + "/2";
         case 'Full Stack Python':
-          return courseLink + "";
+          return courseLink + "/3";
         default:
            return '';
     }
